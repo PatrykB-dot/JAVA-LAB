9bd1970ebcb51ad7a3d98ac83e0a89a988d8036b
PatrykB-dot <patrykfor3@gmail.com>
2022-03-19 15:48:43 +0100

    Added translit method

--- Lab1/src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ Lab1/src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,11 +1,36 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
+import org.apache.commons.lang3.StringUtils;
+import static org.apache.commons.lang3.StringUtils.indexOf;
 
 public class Account {
-      public String name;
-    //  public void setName(String name) {
-        //      this.name = name;
-        //  }
-    //  public String getName() {
-        //      return name;
-        //  }
+    private static final String ukrAlphabet =  "абвгдеєжзиіїйклмнопрстуфхцчшщьюя";
+    private static final String[] translitRepl = {"a","b","v","g","d","e","je","zh","z","y","i","ji", "j",
+            "k","l","m","n","o","p","r","s","t","u","f","h","c","ch","sh","shh","'","ju","ja"};
+    private String name;
+    public String translit(String arg) {
+        var result = new StringBuilder();
+        arg = (arg.toLowerCase());
+        for (int index = 0; index < arg.length(); index++) {
+            var originalLetter = String.valueOf(arg.charAt(index));
+            if (ukrAlphabet.contains(originalLetter)) {
+                String translitLetter =
+                        translitRepl[indexOf(ukrAlphabet, originalLetter)];
+                result.append(translitLetter);
+            } else {
+                result.append(originalLetter);
+            }
+        }
+        return result.toString();
+    }
+        public void setName(String name) {
+            String[] tokens = StringUtils.split(name);
+            for(int i = 0; i < tokens.length; i++)
+                tokens[i] = StringUtils.capitalize(tokens[i]);
+                this.name = StringUtils.join(tokens,' ');
+    }
+    public String getName() {
+            return name;
+        }
 }
+
+
