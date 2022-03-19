6714b229faf685e0c2d6031ac52ced71aaee95bf
PatrykB-dot <patrykfor3@gmail.com>
2022-03-19 14:30:06 +0100

    Comment methods, variable name public

--- Lab1/src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
+++ Lab1/src/pl/lublin/wsei/java/cwiczenia/mylib/Account.java
@@ -1,11 +1,11 @@
 package pl.lublin.wsei.java.cwiczenia.mylib;
 
 public class Account {
-    private String name;
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
+      public String name;
+    //  public void setName(String name) {
+        //      this.name = name;
+        //  }
+    //  public String getName() {
+        //      return name;
+        //  }
 }
