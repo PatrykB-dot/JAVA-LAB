d2df54ba7a5b942c211dca12a042b5e84c1a08ec
PatrykB-dot <patrykfor3@gmail.com>
2022-03-19 14:05:52 +0100

    print dec,hex,bin in main.java

--- Lab1/src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ Lab1/src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -10,16 +10,23 @@ import static java.lang.Integer.toBinaryString;
 import static java.lang.String.*;
 
 public class Main {
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public String leftPad(String aText, char aChar, int aWidth) {
+        String res = aText;
+        for (int i = 0; i< aWidth - aText.length(); i++)
+            res = aChar +res;
+        return res;
+    }
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int num1=0,bin_num=0, hex_num=0;
+        int num1=0;
         do {
             System.out.print("Podaj liczbe ");
             num1 = input.nextInt();
             if (num1 == 0) break;
-
-            System.out.println("Hex string is " + Integer.toHexString(num1));
-            System.out.println("Bin string is " + Integer.toBinaryString(num1));
+            String hex = Integer.toHexString(num1);
+            String bin = Integer.toBinaryString(num1);
+            System.out.println("Hex string is " + leftPad(hex, "%", );
+            System.out.println("Bin string is " + bin);
         } while (true);
     }
 }
