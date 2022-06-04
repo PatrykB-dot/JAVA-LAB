656ce1c361cd1a78530d1aaac017cdc3703bf77e
PatrykB-dot <patrykfor3@gmail.com>
2022-06-04 17:45:25 +0200

    Added TestMyDB

---
+++ Lab3/src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/console/TestMyDB.java
@@ -0,0 +1,15 @@
+package pl.lublin.wsei.java.cwiczenia.lab3.console;
+
+import java.sql.Connection;
+
+public class TestMyDB {
+
+    public static void main(String[] args){
+        MyDB db = new MyDB("localhost", 3306, "mydb");
+        db.setUser("root");
+        db.setPassword("D0ntF0rg3t");
+        Connection conn = db.getConnection();
+        if (conn != null)
+            System.out.println("Połączenie z bazą danych nawiązane");
+    }
+}
