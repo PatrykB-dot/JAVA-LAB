7d9a64bb793b18ce735dcc8dec5666e813802f03
PatrykB-dot <patrykfor3@gmail.com>
2022-06-04 17:49:18 +0200

    closeConnection method test

--- Lab3/src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/console/TestMyDB.java
+++ Lab3/src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/console/TestMyDB.java
@@ -11,5 +11,8 @@ public class TestMyDB {
         Connection conn = db.getConnection();
         if (conn != null)
             System.out.println("Połączenie z bazą danych nawiązane");
+        db.closeConnection();
+        System.out.println("Połączenie z bazą danych zakończone");
     }
+
 }
