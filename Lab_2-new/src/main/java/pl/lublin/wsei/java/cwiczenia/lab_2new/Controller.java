2574685776081bb943a85f0535a60d44a79d0661
PatrykB-dot <patrykfor3@gmail.com>
2022-06-05 18:38:50 +0200

    controller.java

---
+++ Lab_2-new/src/main/java/pl/lublin/wsei/java/cwiczenia/lab_2new/Controller.java
@@ -0,0 +1,29 @@
+package pl.lublin.wsei.java.cwiczenia.lab_2new;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class Controller {
+
+    public Label lbFile;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
+    public void initialize() {
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null){
+            lbFile.setText(file.getAbsolutePath());
+        }
+        else {
+            lbFile.setText("Proszę wczytać plik ...");
+        }
+    }
+}
