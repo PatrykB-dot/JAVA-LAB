de3e09ba667100bc4a01b69014a95e9523e8e802
PatrykB-dot <patrykfor3@gmail.com>
2022-06-05 12:21:24 +0200

    Added GusInfoGraphicList

---
+++ Lab_2-new/src/main/java/pl/lublin/wsei/java/cwiczenia/lab_2new/GusInfoGraphicList.java
@@ -0,0 +1,28 @@
+package pl.lublin.wsei.java.cwiczenia.lab_2new;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class GusInfoGraphicList {
+
+    public ArrayList<Infografika> infografiki;
+
+    public GusInfoGraphicList(String gusFileName) {
+        infografiki = new ArrayList<>();
+        String contents;
+        try {
+            contents = new String(Files.readAllBytes(Paths.get(gusFileName)));
+        }
+        catch (IOException e) {
+            System.out.println("Błąd wczytywania pliku: " +e.getLocalizedMessage());
+            e.printStackTrace();
+            contents = "";
+        }
+        String[] items = contents.split("item");
+        for (int i = 1; i < items.length; i++) {
+            infografiki.add(new Infografika(items[i]));
+        }
+    }
+}
